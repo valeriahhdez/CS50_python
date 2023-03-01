cb2d985ecddf74433f91bdeadf29c10eb100cca6
valeriahhdez <valeriahhdez@gmail.com>
2023-03-01 10:17:39 -0600

    Add deep.py

---
+++ conditionals/deep.py
@@ -0,0 +1,10 @@
+"""
+This program returns 'Yes' whenever the user answers 42 either with number or letters. 
+It is case- and space-insensitive. 
+"""
+
+# Prompt the user to answer the question
+
+# If the answer is 42 o forty two (case- and space-sensitive), print 'Yes'
+
+# Otherwise, print 'No'
