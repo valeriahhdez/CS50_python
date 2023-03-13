695eca8ee00294c5dc6ea20b499e4403d13bf73c
valeriahhdez <valeriahhdez@gmail.com>
2023-03-13 10:08:23 -0600

    Update code comments

--- conditionals/deep.py
+++ conditionals/deep.py
@@ -1,17 +1,17 @@
 """
 This program returns 'Yes' whenever the user answers 42 either with number or letters. 
-It is case- and space-insensitive. 
+It is case- and space-insensitive. It demonstrates the use of the match case statement
 """
 
 # Prompt the user to answer the question
 grt_q = input("What is the answer to the great question of life, the universe, and everything? ")
 
-# Remove all whitespaces and dashes
+# Remove all whitespaces and dashes from input
 def new_word(nswr):
     wrd = nswr.replace(" ", "").replace("-", "").lower()
     return wrd
 
-# Create new variable
+# Store the answer in a new variable
 clean_wrd = new_word(grt_q)
 #print(clean_wrd)
 
