e3e6d50dc34de90633b748d075ecc3cb4ed0d998
valeriahhdez <valeriahhdez@gmail.com>
2023-03-13 09:50:00 -0600

    Add conditionals code

--- conditionals/deep.py
+++ conditionals/deep.py
@@ -4,7 +4,21 @@ It is case- and space-insensitive.
 """
 
 # Prompt the user to answer the question
+grt_q = input("What is the answer to the great question of life, the universe, and everything? ")
 
-# If the answer is 42 o forty two (case- and space-sensitive), print 'Yes'
+# Remove all whitespaces and dashes
+def new_word(nswr):
+    wrd = nswr.replace(" ", "").replace("-", "").lower()
+    return wrd
 
+# Create new variable
+clean_wrd = new_word(grt_q)
+#print(clean_wrd)
+
+# Check answer, if it's 42 or forty two (case- and space-sensitive), print 'Yes'
 # Otherwise, print 'No'
+match clean_wrd:
+    case "fortytwo" | "42":
+        print("Yes!")
+    case _:
+        print("No")
